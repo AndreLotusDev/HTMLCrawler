b0f241d531e6a335517bb844e87b5d12c272f87e
André Soares Gomes <andrsoares953@yahoo.com>
2023-09-20 01:01:00 -0300

    Add project files.

---
+++ HTMLExtracotr/Program.cs
@@ -0,0 +1,24 @@
+﻿using HtmlAgilityPack;
+
+var url = "https://mimetype.io/all-types";
+var web = new HtmlWeb();
+var doc = web.Load(url);
+
+var nodes = doc.DocumentNode.SelectSingleNode("//tbody");
+
+var allFormats = new List<string>();
+
+if (nodes != null)
+{
+    foreach (var tr in nodes.ChildNodes)
+    {
+        var tdMime = tr.ChildNodes.First(f => f.Name == "td");
+        Console.WriteLine(tdMime.InnerText);
+        allFormats.Add(tdMime.InnerText);
+    }
+}
+
+//Write this to a txt file in desktop
+var desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+var path = Path.Combine(desktop, "allFormats.txt");
+File.WriteAllLines(path, allFormats);
